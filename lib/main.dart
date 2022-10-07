cef5daac35ae260868c9eac8d7a99db5fcda060f
Ana Skarica <ana.skarica.as@gmail.com>
2022-10-07 17:39:04 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: const RootPage());
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bottom Navigation"),
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.shifting,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              backgroundColor: Colors.deepPurple),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "User",
+              backgroundColor: Colors.deepPurple),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: "Buy",
+              backgroundColor: Colors.deepPurple),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+              backgroundColor: Colors.deepPurple),
+        ],
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
